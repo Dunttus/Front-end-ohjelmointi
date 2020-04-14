3294995f0dfc343edd08c593b8043c01bde3dea0
Joni Hakala <joni_hakala_92@hotmail.com>
2020-04-15 01:29:32 +0300

    Personal trainer page now has add, delete and edit functions

--- 22Personal_trainer/App.js
+++ 22Personal_trainer/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -10,7 +11,7 @@ function App() {
       <AppBar position="static">
         <Toolbar>
          <Typography variant="h6">
-            Customerlist
+            Customer list
           </Typography>
         </Toolbar>
       </AppBar>
--- 22Personal_trainer/components/Customerlist.js
+++ 22Personal_trainer/components/Customerlist.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import ReactTable from 'react-table-v6'
 import 'react-table-v6/react-table.css'
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Snackbar from '@material-ui/core/Snackbar';
+import Addcustomer from './Addcustomer';
+import Editcustomer from './Editcustomer';
+
 
 export default function Customerlist() {
     const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
         getCustomers();
@@ -14,6 +22,56 @@ export default function Customerlist() {
         .then(response => response.json())
         .then(data => setCustomers(data.content))
         .catch(err => console.error(err))
+    }
+
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')) {
+            fetch(link, {method: 'DELETE'})
+            .then(_ => getCustomers())
+            .then(_ => {
+                setMsg('Customer Deleted');
+                setOpen(true);
+            })
+            .catch(err => console.error(err))
+        }
+    }
+
+    const addCustomer = (customer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(customer)
+            }
+        )
+        .then(_ => getCustomers())
+        .then(_ => {
+            setMsg('New customer added');
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
+    const updateCustomer = (link, customer) => {
+        fetch(link, {
+          method: 'PUT',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(customer)
+        })
+        .then(_ => getCustomers())
+        .then(_ => {
+          setMsg('Customer updated');
+          setOpen(true);
+        })
+        .catch(err => console.error(err))  
+    } 
+
+    const handleClose = () => {
+        setOpen(false);
     }
 
     const columns = [
@@ -34,18 +92,35 @@ export default function Customerlist() {
             accessor: 'phone'
         },
         {
-            Header: 'Streetaddress',
+            Header: 'Address',
             accessor: 'streetaddress'
         },
         {
             Header: 'City',
             accessor: 'city'
+        },
+        {
+            Cell: row => (<Editcustomer customer={row.original} updateCustomer={updateCustomer} />)
+        },
+        {
+            Cell: row => (<Button startIcon={<DeleteIcon />} color="secondary" onClick={() => deleteCustomer(row.original.links[0].href)}>Delete</Button>)
         }
     ]
 
     return(
         <div>
+            <Addcustomer addCustomer={addCustomer}/>
             <ReactTable defaultPageSize={10}  filterable={true} data={customers} columns={columns} />
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                message={msg}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left'                
+                }}
+            />
         </div>
     )
 }
